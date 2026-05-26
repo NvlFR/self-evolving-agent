153a726cd77629f7305b5901fd5d75e6d27e56d0
Noval Faturrahman <novalfr802@gmail.com>
2026-05-26 14:51:33 +0700

    Add planner module

---
+++ brain/planner.py
@@ -0,0 +1,8 @@
+class Planner:
+    def create_plan(self, goal: str):
+        return [
+            f"analyze goal: {goal}",
+            "generate solution",
+            "execute solution",
+            "evaluate result"
+        ]
